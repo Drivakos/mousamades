dc2f79173a2a08c0321717df08b8d91b25c3d345
Drivakos <ciedas99@gmail.com>
2023-09-26 16:16:00 +0300

    Feat: added socials on header and worked on header styles WIP

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <HeaderComponent></HeaderComponent>
+    <BodyComponent></BodyComponent>
   </div>
 </template>
--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,5 +1,19 @@
 <template>
-test
+  <div class="header-wrapper">
+    <img src="/mousamades%20logo.png" alt="Mousamades Drivakos logo" class="site-logo">
+    <div class="hidden-mobile"></div>
+    <div class="contact-info-wrapper">
+      <a href="tel:+2105550554">
+        <img src="/phone-logo.svg" alt="Phone icon" class="site-logo">
+      </a>
+      <a href="https://maps.app.goo.gl/5LkJShPSesoPHMVD8" target="_blank">
+        <img src="/maps-logo.svg" alt="Maps icon" class="site-logo">
+      </a>
+      <a href="https://www.facebook.com/profile.php?id=100004211236146" target="_blank">
+        <img src="/facebook-icon.svg" alt="Facebook icon" class="site-logo">
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
