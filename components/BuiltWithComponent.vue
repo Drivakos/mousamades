336b7e843fc89bbb8ac2a9f3a4f25efc39862826
Drivakos <ciedas99@gmail.com>
2023-09-27 16:00:40 +0300

    Feat: added content/images to build with section

--- components/BuiltWithComponent.vue
+++ components/BuiltWithComponent.vue
@@ -1,5 +1,15 @@
 <template>
-  built with
+  <div class="build-with-title">ΥΛΙΚΑ</div>
+  <div class="build-with-wrapper">
+    <a href="https://www.mehler-texnologies.com/en/produkte-archive/tarp-fr/" class="slide">
+      <img src="/build-with-assets/melher-logo.png" alt="Mousamades Drivakos logo" class="site-logo">
+      <div class="slide-text">ΥΛΙΚΑ <br> ΜΟΥΣΑΜΑΔΩΝ</div>
+    </a>
+    <a class="slide">
+      <img src="/build-with-assets/stickers.png" alt="Mousamades Drivakos logo" class="site-logo">
+      <div class="slide-text"> ΚΟΠΤΙΚΑ <br> ΑΥΤΟΚΟΛΛΗΤΑ</div>
+    </a>
+  </div>
 </template>
 
 <script>
