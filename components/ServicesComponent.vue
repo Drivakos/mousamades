1221541d34a0ea197c4a3cb945c507a00c38dd23
Drivakos <ciedas99@gmail.com>
2023-09-27 16:40:05 +0300

    Feat: added content to ServicesComponent

--- components/ServicesComponent.vue
+++ components/ServicesComponent.vue
@@ -1,5 +1,105 @@
 <template>
-services component
+  <div class="services-title">ΥΠΗΡΕΣΙΕΣ</div>
+  <div class="services-wrapper">
+    <div class="services-text-box">
+      <span>Μουσαμάδες Φορτηγών:</span>
+      <ul>
+        <li>
+          Τύπου Κουρτίνας
+        </li>
+        <li>
+          TIR
+        </li>
+        <li>
+          Συρόμενοι για ανατροπές <br> (χωματουργικά)
+        </li>
+      </ul>
+    </div>
+
+    <div class="services-text-box">
+      <span>Αξεσουάρ:</span>
+      <ul>
+        <li>
+          Κλειδιά Κουρτίνας
+        </li>
+        <li>
+          Ράουλα
+        </li>
+        <li>
+          Χούφτες Αλουμινίου
+        </li>
+        <li>
+          Σωλήνες Αλουμινίου
+        </li>
+        <li>
+          Καρυδάκια
+        </li>
+        <li>
+          Ιμάντες Κλειδιών
+        </li>
+      </ul>
+    </div>
+
+    <div class="services-text-box">
+      <span>Επιγραφές:</span>
+      <ul>
+        <li>
+          Ψηδιακές εκτυπώσεις και <br> Επιγραφές με αερογράφο
+        </li>
+      </ul>
+      <span>Ντύσιμο Αυτοκινήτου:</span>
+      <ul>
+        <li>
+          Αυτοκόλλητα Βινυλίου <br> (3ετίας, 5ετίας, 7ετίας)
+        </li>
+      </ul>
+    </div>
+
+    <div class="services-text-box">
+      <span>Καλύψεις κτιρίων:</span>
+      <ul>
+        <li>
+          Βιομηχανικών Χώρων
+        </li>
+        <li>
+          Αποθηκευτικών Χώρων
+        </li>
+        <li>
+          Parking
+        </li>
+        <li>
+          Στέγαστρα Μεγάλων Επιφανειών <br> από Μουσαμά
+        </li>
+        <li>
+          Αίθρια Καφετεριών & Εστιατορίων
+        </li>
+      </ul>
+    </div>
+
+    <div class="services-text-box">
+      <span>Καλύμματα:</span>
+      <ul>
+        <li>
+          Καλύμματα Σκαφών
+        </li>
+        <li>
+          Καλύμματα Parking
+        </li>
+        <li>
+          Τεντάκια Σκίασης
+        </li>
+      </ul>
+    </div>
+
+    <div class="services-text-box">
+      <span>Ταμπέλες:</span>
+      <ul>
+        <li>
+          Καταστημάτων και Εταιριών με Μουσαμά
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
