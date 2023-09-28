a96a362797a16b4f2219fc09a8a178bcf4e4624b
Drivakos <ciedas99@gmail.com>
2023-09-28 15:50:24 +0300

    Fix: refactored paralax class name, Feat: added hover effect on built-with images

--- components/BuiltWithComponent.vue
+++ components/BuiltWithComponent.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="build-with-outer-wrapper parallax-services">
+  <div class="build-with-outer-wrapper parallax-built-with">
     <div class="build-with-title">ΥΛΙΚΑ</div>
     <div class="build-with-wrapper">
       <a href="https://www.mehler-texnologies.com/en/produkte-archive/tarp-fr/" class="slide">
-        <img src="/build-with-assets/melher-logo.png" alt="Mousamades Drivakos logo" class="site-logo">
+        <img src="/build-with-assets/melher-logo.png" alt="Mousamades Drivakos logo" class="built-with-logo">
         <div class="slide-text">ΥΛΙΚΑ <br> ΜΟΥΣΑΜΑΔΩΝ</div>
       </a>
       <a class="slide">
-        <img src="/build-with-assets/stickers.png" alt="Mousamades Drivakos logo" class="site-logo">
+        <img src="/build-with-assets/stickers.png" alt="Mousamades Drivakos logo" class="built-with-logo">
         <div class="slide-text"> ΚΟΠΤΙΚΑ <br> ΑΥΤΟΚΟΛΛΗΤΑ</div>
       </a>
     </div>
