f92b54c682db499ad1f6fba931d64ed23b7457b0
Drivakos <ciedas99@gmail.com>
2023-09-28 14:38:13 +0300

    Feat: Added mainslider description

--- components/BuiltWithComponent.vue
+++ components/BuiltWithComponent.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="build-with-title">ΥΛΙΚΑ</div>
-  <div class="build-with-wrapper">
-    <a href="https://www.mehler-texnologies.com/en/produkte-archive/tarp-fr/" class="slide">
-      <img src="/build-with-assets/melher-logo.png" alt="Mousamades Drivakos logo" class="site-logo">
-      <div class="slide-text">ΥΛΙΚΑ <br> ΜΟΥΣΑΜΑΔΩΝ</div>
-    </a>
-    <a class="slide">
-      <img src="/build-with-assets/stickers.png" alt="Mousamades Drivakos logo" class="site-logo">
-      <div class="slide-text"> ΚΟΠΤΙΚΑ <br> ΑΥΤΟΚΟΛΛΗΤΑ</div>
-    </a>
+  <div class="build-with-outer-wrapper">
+    <div class="build-with-title">ΥΛΙΚΑ</div>
+    <div class="build-with-wrapper">
+      <a href="https://www.mehler-texnologies.com/en/produkte-archive/tarp-fr/" class="slide">
+        <img src="/build-with-assets/melher-logo.png" alt="Mousamades Drivakos logo" class="site-logo">
+        <div class="slide-text">ΥΛΙΚΑ <br> ΜΟΥΣΑΜΑΔΩΝ</div>
+      </a>
+      <a class="slide">
+        <img src="/build-with-assets/stickers.png" alt="Mousamades Drivakos logo" class="site-logo">
+        <div class="slide-text"> ΚΟΠΤΙΚΑ <br> ΑΥΤΟΚΟΛΛΗΤΑ</div>
+      </a>
+    </div>
   </div>
 </template>
 
