d5a15dde2c179e018a058f0a65554d00497c6b1b
Drivakos <ciedas99@gmail.com>
2023-09-28 13:02:57 +0300

    Feat: Populated contactComponent section, Fix: fixed oveflowing issues on mobile

--- components/ContactComponent.vue
+++ components/ContactComponent.vue
@@ -1,5 +1,32 @@
 <template>
-  contact
+  <div class="contact-section-title">ΕΠΙΚΟΙΝΩΝΙΑ</div>
+  <div class="contact-section-wrapper">
+    <div class="contact-section-text">
+      <div class="contact-section-text-subtitle">ΣΤΟΙΧΕΙΑ ΕΠΙΚΟΙΝΩΝΙΑΣ</div>
+      <div class="text-line">
+        <a href="mailto:mousamadesdrivakos@gmail.com">
+          <img src="/mail-icon.svg" alt="Mousamades Petros Drivakos mail" class="">
+        </a>
+        <div>
+          <a href="mailto:mousamadesdrivakos@gmail.com">
+            mousamadesdrivakos@gmail.com <br>
+            info@mousamadesdrivakos.gr
+          </a>
+        </div>
+      </div>
+      <div class="text-line">
+        <a href="tel:+2105550554">
+          <img src="/phone-logo.svg" alt="Mousamades Petros Drivakos phone" class="">
+        </a>
+        <div>
+          <a href="tel:+2105550554">210-5550554</a>
+        </div>
+      </div>
+    </div>
+    <div class="iframe-map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3141.5509009131742!2d23.5211348!3d38.05754950000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1ae67652bcc61%3A0x41fd4a3426814bc2!2zzpzOn86lzqPOkc6czpHOlM6VzqMgzqDOlc6kzqHOn86jIM6UzqHOmc6SzpHOms6fzqM!5e0!3m2!1sel!2sgr!4v1695893892194!5m2!1sel!2sgr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
 </template>
 
 <script>
