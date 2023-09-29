eb171e531b912254bd1b53802f35f817290ba820
Drivakos <ciedas99@gmail.com>
2023-09-29 14:09:21 +0300

    Fix: Refactored mainslider title text to be absolute over the main slider images

--- components/MainSliderComponent.vue
+++ components/MainSliderComponent.vue
@@ -1,23 +1,28 @@
 <template>
-  <carousel :items-to-show="1" :autoplay="4000" :wrap-around="true" :transition="1500" class="main-slider-wrapper">
-    <slide v-for="slide in 6" :key="slide" :autoplay="true">
-      <img :src="`/mainSlider/${slide}.jpg`" alt="main slider image" class="main-slider-img" />
-    </slide>
-  </carousel>
-  <div class="main-slider-text">
-    ΜΟΥΣΑΜΑΔΕΣ <br>
-    ΕΠΙΓΡΑΦΕΣ <br>
-    <span>ΠΕΤΡΟΣ ΔΡΙΒΑΚΟΣ</span>
+  <div class="pos-rel">
+    <carousel :items-to-show="1" :autoplay="4000" :wrap-around="true" :transition="1500" class="main-slider-wrapper">
+      <slide v-for="slide in 6" :key="slide" :autoplay="true">
+        <img :src="`/mainSlider/${slide}.jpg`" alt="main slider image" class="main-slider-img"/>
+      </slide>
+    </carousel>
+    <div class="main-slider-text">
+      ΜΟΥΣΑΜΑΔΕΣ <br>
+      ΕΠΙΓΡΑΦΕΣ <br>
+      <span>ΠΕΤΡΟΣ ΔΡΙΒΑΚΟΣ</span>
+    </div>
+
   </div>
   <div class="main-slider-description">
-    Η εταιρεία μας ιδρύθηκε το 1969, από τον Δημ. Δριβάκο. Δραστηριοποιείται στον χώρο κατασκευής Μουσαμάδων, Φορτηγών Αυτοκινήτων, Υπόστεγων Κάλυψης Χώρων, Επιγραφών και Αυτοκόλλητων.
-    <br>Το 2014 αναλαμβάνει ο υιός Πέτρος Δριβάκος. Ο συνδυασμός του πιο σύγχρονου μηχανολογικού εξοπλισμού και της πολυετής πείρας, παράλληλα με τις λογικές τιμές, αποτελούν εγγύηση για κάθε εργασία που αναλαμβάνουμε.
+    Η εταιρεία μας ιδρύθηκε το 1969, από τον Δημ. Δριβάκο. Δραστηριοποιείται στον χώρο κατασκευής Μουσαμάδων, Φορτηγών
+    Αυτοκινήτων, Υπόστεγων Κάλυψης Χώρων, Επιγραφών και Αυτοκόλλητων.
+    <br>Το 2014 αναλαμβάνει ο υιός Πέτρος Δριβάκος. Ο συνδυασμός του πιο σύγχρονου μηχανολογικού εξοπλισμού και της
+    πολυετής πείρας, παράλληλα με τις λογικές τιμές, αποτελούν εγγύηση για κάθε εργασία που αναλαμβάνουμε.
   </div>
 </template>
 
 <script>
 import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import {Carousel, Slide, Pagination, Navigation} from 'vue3-carousel'
 
 export default {
   name: 'App',
