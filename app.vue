1a79755e6be417c90b90f7222f19349fd9cfb71c
Drivakos <vdrivakos@advisable.com>
2023-12-12 21:49:58 +0200

    Fix(Vercel analytics) : Fixed vercel analytics import

--- app.vue
+++ app.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 
 useHead({
   title: 'ΜΟΥΣΑΜΑΔΕΣ ΠΕΤΡΟΣ ΔΡΙΒΑΚΟΣ',
